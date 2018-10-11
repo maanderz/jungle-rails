f3f4d0ab98e07e9a794b370cba90e4266ef78b6e
Mandy Fung <mandyfung93@gmail.com>
2018-10-11 02:41:43 +0000

    added signin/login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -19,10 +19,7 @@ class SessionsController < ApplicationController
 
   def destroy
     session[:user_id] = nil
-    redirect_to '/login'
+    redirect_to '/'
   end 
-
-  def destroy 
-  end
 
 end

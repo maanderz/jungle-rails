f0404e0ba27e08fd11fbea35d512b7cb20439c57
Mandy Fung <mandyfung93@gmail.com>
2018-10-12 00:10:46 +0000

    security added

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,10 @@
 class Admin::DashboardController < ApplicationController
+  http_basic_authenticate_with name: ENV['ADMIN_USER'], password: ENV['ADMIN_PASSWORD']
+  
+  def index 
+    render plain: "hello"
+  end
+
   def show
   end
 end

c84d137e4ff40ea033b038c3becaa1ca232e7287
Mandy Fung <mandyfung93@gmail.com>
2018-10-12 23:11:23 +0000

    delete button

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,19 +1,23 @@
 class ReviewsController < ApplicationController
 
+  skip_before_action :require_login, only: [:review]
+
   def create
     @product = Product.find params[:product_id]
     @review = @product.reviews.new(reviews_params)
     @review.user_id = current_user.id
-
-    if !@review.valid?
-      p @review.errors.messages
-    end
 
     if @review.save
       redirect_to product_path(@product)
     else
       redirect_to product_path(@product)
     end
+  end
+
+  def destroy
+    @review = Review.find params[:id]
+    @review.destroy
+    redirect_to "/"
   end
 
   private
@@ -24,6 +28,15 @@ class ReviewsController < ApplicationController
       :rating
     )
   end
+
+  def require_login
+    unless logged_in?
+      flash[:error] = "You must be logged in to access this section"
+      redirect_to product_path(@product)
+    end
+  end
+
 end
+
 
 

aae95729145d122c6b2320578178e93da7053843
Mandy Fung <mandyfung93@gmail.com>
2018-10-13 00:10:26 +0000

    email bug fixed

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,6 +4,6 @@ class UserMailer < ApplicationMailer
   def confirmation_email(current_user, order)
     @order = order
     @current_user = current_user
-    mail(to: @current_user.email, subject: @order.id)
+    mail(to: @current_user.email)
   end
 end
